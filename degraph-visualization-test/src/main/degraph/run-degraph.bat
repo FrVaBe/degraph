b76cfa76a9aa0d28e8f704075d693e4d1636806b
Michael J. Simons <michael@simons.ac>
2017-02-25 16:37:10 +0100

    Remove batch file and generate graphs through maven
    Run with `mvn clean package` to get desired graphs.
    
    Note: Ask @schauder to put degraph 0.1.4 on central, so that it uses the most recent version.

--- degraph-visualization-test/src/main/degraph/run-degraph.bat
+++
@@ -1,9 +0,0 @@
-rem
-rem build project and run degraph
-rem Maven and Degraph need to be installed and on the PATH
-rem
-
-call mvn -f ../../../pom.xml clean install
-mkdir ..\..\..\target\degraph
-call degraph -f packages.config
-call degraph -f packagesWithExclude.config
