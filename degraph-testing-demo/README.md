0ae3f7bc9421d5e2a6887483b947d0f6988dedb9
Franz van Betteraey <fvbetteraey@web.de>
2017-03-08 22:24:48 +0100

    add tests and update README

--- degraph-testing-demo/README.md
+++ degraph-testing-demo/README.md
@@ -2,6 +2,10 @@
 
 A sample project which uses `degraph-check` to write unit tests that check package dependencies.
 
+The package dependencies of this project look like that:
+
+![package dependencies](src/main/degraph/yEd/packages.png)
+
 This project mainly demonstrates
 * which project dependencies are needed (Degraph, Hamcrest and JUnit)  
 [see [pom.xml](pom.xml)]
@@ -11,3 +15,7 @@ This project mainly demonstrates
 **Links**
 * [Degraph](http://blog.schauderhaft.de/degraph/)
 * Degraph - [Testing of Dependencies](http://blog.schauderhaft.de/degraph/documentation.html#testing-of-dependencies)
+
+**Pro Top**
+
+If you run into an `IllegalArgumentException` while testing your code base, you possibly have the wrong `asm` version on your classpath (see Degraph issue [#82](https://github.com/schauder/degraph/issues/82)). In this case the easiest workaround is to move the degraph tests into a separate module.
--- degraph-testing-demo/src/test/java/de/frvabe/PackageDependenciesTest.java
+++ degraph-testing-demo/src/test/java/de/frvabe/PackageDependenciesTest.java
@@ -12,7 +12,7 @@ import de.schauderhaft.degraph.configuration.NamedPattern;
 public class PackageDependenciesTest {
 
     /**
-     * Check if the packages of the module classpath are violation free (slices are implicit define
+     * Checks if the packages of the module classpath are violation free (slices are implicit define
      * by the packages).
      */
     @Test(expected = AssertionError.class)
@@ -21,7 +21,7 @@ public class PackageDependenciesTest {
     }
 
     /**
-     * Check if the packages of the module are violation free (slices are explicit defined).
+     * Checks if the packages of the module are violation free (slices are explicit defined).
      * <p>
      * Disclaimer: I am not sure of the implicit package slices still exists even if explicit
      * slicing is used.
@@ -32,14 +32,14 @@ public class PackageDependenciesTest {
         assertThat(classpath()
                 .including("de.frvabe.**")
                 .withSlicing("packages",
-                        "de.frvabe.(*).**",
-                        new NamedPattern("de.frvabe.*", "base"), 
-                        new NamedPattern("de.frvabe.b.c.*", "c")),
+                        new NamedPattern("de.frvabe.*", "base"),
+                        "de.frvabe.(*).*",
+                        "de.frvabe.b.(*).*"),
                 is(violationFree()));
     }
 
     /**
-     * Check a dependency path between two packages that is considered to be valid.
+     * Checks a dependency path between two packages that is considered to be valid.
      * <p>
      * Only the two relevant packages are included in the classpath because otherwise the invalid
      * package cycle of the overall project would make this test fail.
@@ -51,15 +51,14 @@ public class PackageDependenciesTest {
                 .including("de.frvabe.a.*")
                 .including("de.frvabe.b.*")
                 .withSlicing("packages",
-                        "de.frvabe.(*).**",
-                        new NamedPattern("de.frvabe.*", "base"),
-                        new NamedPattern("de.frvabe.b.c.*", "c"))
+                        "de.frvabe.(*).*")
+                // dependency from left to right is OK; from right to left is not OK
                 .allow("b", "a"),
                 is(violationFree()));
     }
 
     /**
-     * Check a dependency path between two packages that is considered to be valid (as it is the
+     * Checks a dependency path between two packages that is considered to be valid (as it is the
      * opposite path of {@link #validDependency()} an exception is expected in this test.
      * <p>
      * Only the two relevant packages are included in the classpath because otherwise the invalid
@@ -72,10 +71,53 @@ public class PackageDependenciesTest {
                 .including("de.frvabe.a.*")
                 .including("de.frvabe.b.*")
                 .withSlicing("packages",
-                        "de.frvabe.(*).**",
-                        new NamedPattern("de.frvabe.*", "base"),
-                        new NamedPattern("de.frvabe.b.c.*", "c"))
+                        "de.frvabe.(*).*")
+                // dependency from left to right is OK; from right to left is not OK
                 .allow("a", "b"),
+                is(violationFree()));
+    }
+    
+    /**
+     * Checks a dependency path between several packages that is considered to be valid.
+     * <p>
+     * Only the relevant packages are included in the classpath because otherwise the invalid
+     * package cycle of the overall project would make this test fail.
+     * </p>
+     */
+    @Test
+    public void dependencyChain() {
+        assertThat(classpath()
+                .including("de.frvabe.a.*")
+                .including("de.frvabe.b.*")
+                .including("de.frvabe.b.c.*")
+                .withSlicing("packages",
+                        "de.frvabe.(*).*",
+                        "de.frvabe.b.(*).*")
+                // dependencies from left to right are OK; from right to left are not OK
+                .allow("c", "b", "a"),
+                is(violationFree()));
+    }
+
+    /**
+     * Checks a strict dependency path between several packages. As {@code class C2} depends on
+     * {@code class A1} while skipping the {@code package b} an exception is expected.
+     * <p>
+     * Only the two relevant packages are included in the classpath because otherwise the invalid
+     * package cycle of the overall project would make this test also fail.
+     * </p>
+     */
+    @Test(expected = AssertionError.class)
+    public void strictDependencyChain() {
+        assertThat(classpath()
+                .including("de.frvabe.a.*")
+                .including("de.frvabe.b.*")
+                .including("de.frvabe.b.c.*")
+                .withSlicing("packages",
+                        "de.frvabe.(*).*",
+                        "de.frvabe.b.(*).*")
+                // c is allowed to depend on b (to skip a layer is not allowed)
+                // b is allowed to depend on a
+                .allowDirect("c", "b", "a"),
                 is(violationFree()));
     }
 
